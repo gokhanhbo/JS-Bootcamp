e7279cb51e0a403e8ee922ad92c4728771bd0e6b
gokhanhbo <gokhanhbo@gmail.com>
2021-08-30 17:07:07 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const btn = document.querySelector('button')
+
+btn.addEventListener('click', function() {
+    document.body.style.backgroundColor = "olivedrab";
+    console.log("Hello World")
+})
